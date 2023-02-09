fed0db149bb9c4e5082f3128be19442814d92047
L.S <1157723200@qq.com>
2023-02-09 15:47:17 +0800

    增加高级功能

---
+++ innerLogic.py
@@ -0,0 +1,191 @@
+from innerui import Ui_Dialog
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import *
+import sys
+
+# import glo
+
+# glo.__init__()
+
+
+class Info:
+
+    site_info = {
+        "tb": r"https://www.taobao.com/",
+        "jd": r"https://www.jd.com/?cu=true",
+        "zhihu": r"https://www.zhihu.com/",
+        "gitee": r"https://gitee.com/",
+        "github": r"https://github.com/",
+        "csdn": r"https://www.csdn.net/",
+    }
+
+
+class dialog(QDialog, Ui_Dialog, Info):
+    def __init__(self):
+
+        __adStatus = True
+
+        super(dialog, self).__init__()
+
+        # self.setWindowTitle('Advanced Options')
+
+        # self.setupUi(self)
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setupUi(self)
+
+        self.setWindowTitle("Advanced Options")
+
+        self.file_type = None
+        self.site = None
+        self.all_kw = False
+        self.ad_close = False
+
+        self.checkBox_site.setTristate(True)
+
+        self.checkBox_site.setCheckState(Qt.PartiallyChecked)
+
+        self.sub_box = [
+            self.checkBox_tb,
+            self.checkBox_jd,
+            self.checkBox_gitee,
+            self.checkBox_zhihu,
+            self.checkBox_github,
+            self.checkBox_cdsn,
+        ]
+
+        site_info = Info().site_info
+        # 定义触发
+
+        self.checkBox_filetype.stateChanged.connect(
+            lambda: self.filetype(self.checkBox_filetype)
+        )
+
+        self.checkBox_kw.stateChanged.connect(self.set_allkw)
+
+        self.checkBox_site.stateChanged.connect(self.choose_site)
+
+        self.checkBox_closead.stateChanged.connect(self.close_add)
+
+        for x in self.sub_box:
+            x.stateChanged.connect(self.set_site)
+
+        self.buttonBox.accepted.connect(self.confirm)
+        self.buttonBox.rejected.connect(self.cancled)
+
+    # 重置所有subbox
+
+    @staticmethod
+    def set_sub_false(sub_list: list):
+        for item in sub_list:
+            item.setCheckState(False)
+            item.setCheckable(False)
+
+    @staticmethod
+    def set_sub_true(sub_list: list):
+        for item in sub_list:
+            item.setCheckState(0)
+            item.setCheckable(True)
+
+    # 定义全局逻辑关系
+
+    def judger(self):
+
+        if self.checkBox_site.checkState() != 2:
+
+            self.set_sub_false(self.sub_box)
+        else:
+            self.set_sub_true(self.sub_box)
+
+        # print(self.checkBox_site.checkState())
+
+    # 注意高级功能的逻辑关系；首先添加关键字，最后加上引号
+    def set_site(self):
+        temp_str = " site:"
+        if self.checkBox_tb.isChecked():
+            self.site = temp_str + self.site_info["tb"]
+        elif self.checkBox_jd.isChecked():
+            self.site = temp_str + self.site_info["jd"]
+        elif self.checkBox_zhihu.isChecked():
+            self.site = temp_str + self.site_info["zhihu"]
+        elif self.checkBox_gitee.isChecked():
+            self.site = temp_str + self.site_info["gitee"]
+        elif self.checkBox_github.isChecked():
+            self.site = temp_str + self.site_info["github"]
+        elif self.checkBox_cdsn.isChecked():
+            self.site = temp_str + self.site_info["csdn"]
+        else:
+            self.site = None
+
+    def choose_site(self):
+        self.judger()
+        self.set_site()
+
+    def filetype(self, e):
+        if e.text() == "限定网站":
+            self.judger()
+
+        # print(e.text())
+
+        if self.checkBox_filetype.isChecked():
+
+            self.file_type = " filetype:pdf"
+
+        else:
+
+            self.file_type = None
+
+    def set_allkw(self):
+        if self.checkBox_kw.isChecked():
+            self.all_kw = True
+        else:
+            self.all_kw = False
+
+    # 未点击时默认开，防止过度点击。该变量不可访问.
+    def close_add(self):
+
+        if self.checkBox_closead.isChecked():
+
+            self.ad_close = True
+
+        else:
+
+            self.ad_close = False
+
+    # 确定关闭窗口
+    def generate_cofigure(self):
+        config = {
+            "ad_close": self.ad_close,
+            "all_kw": self.all_kw,
+            "site": self.site,
+            "filetype": self.file_type,
+        }
+        # glo.set_value('config', config)
+        return config
+
+    def init_params(self):
+        self.file_type = None
+        self.site = None
+        self.all_kw = False
+        self.ad_close = False
+
+    def confirm(self):
+
+        return self.generate_cofigure()
+
+    def cancled(self):
+        self.init_params()
+        return None
+        # self.close()
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ex = dialog()
+    ex.show()
+    sys.exit(app.exec_())
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ from PyQt5.QtWidgets import *
 from config import Config
 from get_img import BackGroundPic
 from searchui import Ui_MainWindow
+from innerLogic import dialog
+import glo
 
 
 class main(QMainWindow, Ui_MainWindow):
@@ -20,12 +22,11 @@ class main(QMainWindow, Ui_MainWindow):
         self.initUI(self.backgroundpic.current_pic, self.configfile)
 
     def initUI(self, backgraoundpic: str, config: Config):
-        self.setStyle(QStyleFactory.create('Fusion'))
+        self.setStyle(QStyleFactory.create("Fusion"))
 
         # 设置背景
         if config.back_enable:
-            self.setStyleSheet(
-                f"#MainWindow{{border-image:url({backgraoundpic})}}")
+            self.setStyleSheet(f"#MainWindow{{border-image:url({backgraoundpic})}}")
 
         self.setupUi(self)
         # self.baidu.setChecked(True)
@@ -33,33 +34,43 @@ class main(QMainWindow, Ui_MainWindow):
         self.baidu.stateChanged.connect(lambda: self.search_url(self.baidu))
         self.bing.stateChanged.connect(lambda: self.search_url(self.bing))
         self.quark.stateChanged.connect(lambda: self.search_url(self.quark))
-
-        self.keyword = ''
+        self.toolButton.clicked.connect(self.AdvancedOptions)
+        self.keyword = ""
         self.page_all_num = 20
         self.page_num = 2
+        self.cofig = None
 
-        self.search_url_dic = {'baidu': r'https://www.baidu.com/s',
-                               'bing': r'https://cn.bing.com/search',
-                               'quark': r'https://quark.sm.cn/s'}
-        self.url = self.search_url_dic['baidu']
-        self.linkXpath = {'baidu': r'//div[@class="result c-container xpath-log new-pmd"]',
-                          'bing': r'//main/ol/li[@class="b_algo"]',
-                          'quark': r'/html/body//div[@id="results"]/div'}
+        self.search_url_dic = {
+            "baidu": r"https://www.baidu.com/s",
+            "bing": r"https://cn.bing.com/search",
+            "quark": r"https://quark.sm.cn/s",
+        }
+        self.url = self.search_url_dic["baidu"]
+        self.linkXpath = {
+            "baidu": r'//div[@class="result c-container xpath-log new-pmd"]',
+            "bing": r'//main/ol/li[@class="b_algo"]',
+            "quark": r'/html/body//div[@id="results"]/div',
+        }
 
-        self.subxpath = {'baidu': r'.//h3/a',
-                         'bing': [r'.//div[@class="b_title"]/h2/a', r'.//div[@class="b_algoheader"]/a'],
-                         'quark': r'./div[1]/a'}
+        self.subxpath = {
+            "baidu": r".//h3/a",
+            "bing": [
+                r'.//div[@class="b_title"]/h2/a',
+                r'.//div[@class="b_algoheader"]/a',
+            ],
+            "quark": r"./div[1]/a",
+        }
 
-        self.engine_params = {'baidu': {'wd': self.keyword, 'pn': 1, 'tn': 'baiduhome', 'ie': 'utf-8'},
-                              'bing': {'q': self.keyword, 'first': 1},
-                              'quark': {'q': self.keyword, 'snum': '10', 'page': 1}
-                              }
+        self.engine_params = {
+            "baidu": {"wd": self.keyword, "pn": 1, "tn": "baiduhome", "ie": "utf-8"},
+            "bing": {"q": self.keyword, "first": 1},
+            "quark": {"q": self.keyword, "snum": "10", "page": 1},
+        }
 
         self.headers = {
-            'User-Agent':
-            'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
-AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg/109.0.1518.55',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) \
+AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg/109.0.1518.55",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         }
 
         self.lineEdit.textChanged.connect(self.setkeyword)
@@ -69,7 +80,8 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg
         self.linklist = []
 
     @staticmethod
-    def rule(x): return x//10+1 if x % 10 != 0 else x//10
+    def rule(x):
+        return x // 10 + 1 if x % 10 != 0 else x // 10
 
     def setkeyword(self, keyword):
         self.keyword = keyword
@@ -82,38 +94,39 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg
     def search_url(self, e):
         print(e)
         print(e.text())
-        if e.text() == '百度':
-            self.url = self.search_url_dic['baidu']
-        elif e.text() == '必应':
-            self.url = self.search_url_dic['bing']
+        if e.text() == "百度":
+            self.url = self.search_url_dic["baidu"]
+        elif e.text() == "必应":
+            self.url = self.search_url_dic["bing"]
         else:
-            self.url = self.search_url_dic['quark']
+            self.url = self.search_url_dic["quark"]
 
     @staticmethod
     def get_list(**kwargs) -> list:
         result = []
         r = requests.get(
-            url=kwargs['url'], headers=kwargs['headers'], params=kwargs['params'])
+            url=kwargs["url"], headers=kwargs["headers"], params=kwargs["params"]
+        )
 
-        with open('test.html', 'wb') as f:
+        with open("test.html", "wb") as f:
             f.write(r.content)
 
         t = etree.HTML(r.content)
 
-        print(kwargs['linkXpath'])
-        selector = t.xpath(kwargs['linkXpath'])
+        print(kwargs["linkXpath"])
+        selector = t.xpath(kwargs["linkXpath"])
         print(selector)
 
         for item in selector:
             # link = item.xpath(kwargs['subxpath'])
             # print(link)
             print(item)
-            if len(kwargs['subxpath']) != 1:
+            if len(kwargs["subxpath"]) != 1:
                 try:
-                    for rule in kwargs['subxpath']:
+                    for rule in kwargs["subxpath"]:
                         print(rule)
 
-                        link = item.xpath(rule)[0].get('href')
+                        link = item.xpath(rule)[0].get("href")
                         print(link)
                         result.append(link)
                 except Exception as e:
@@ -123,7 +136,7 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg
                 # finally:
                 #     continue
             else:
-                link = item.xpath(kwargs['subxpath'])[0].get('href')
+                link = item.xpath(kwargs["subxpath"])[0].get("href")
                 print(link)
                 result.append(link)
 
@@ -138,39 +151,44 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg
     def search(self):
 
         if self.baidu.isChecked():
-            self.engine_params['baidu']['wd'] = self.keyword
+            self.engine_params["baidu"]["wd"] = self.keyword
             for page in range(self.page_num):
-                self.engine_params['baidu']['pn'] = str(page*10+1)
+                self.engine_params["baidu"]["pn"] = str(page * 10 + 1)
 
                 r = requests.get(
-                    url=self.url, headers=self.headers, params=self.engine_params['baidu'])
+                    url=self.url,
+                    headers=self.headers,
+                    params=self.engine_params["baidu"],
+                )
 
                 t = etree.HTML(r.content)
-                selector = t.xpath(self.linkXpath['baidu'])
+                selector = t.xpath(self.linkXpath["baidu"])
                 print(selector)
                 for item in selector:
-                    link = item.xpath(self.subxpath['baidu'])[0].get('href')
+                    link = item.xpath(self.subxpath["baidu"])[0].get("href")
 
                     self.linklist.append(link)
-        
 
         elif self.bing.isChecked():
 
-            self.engine_params['bing']['q'] = self.keyword
+            self.engine_params["bing"]["q"] = self.keyword
             for page in range(self.page_num):
-                self.engine_params['baidu']['first'] = str(page*10+1)
+                self.engine_params["baidu"]["first"] = str(page * 10 + 1)
 
                 r = requests.get(
-                    url=self.url, headers=self.headers, params=self.engine_params['bing'])
+                    url=self.url,
+                    headers=self.headers,
+                    params=self.engine_params["bing"],
+                )
 
                 t = etree.HTML(r.content)
-                selector = t.xpath(self.linkXpath['bing'])
+                selector = t.xpath(self.linkXpath["bing"])
 
-                for i in self.subxpath['bing']:
+                for i in self.subxpath["bing"]:
                     try:
                         for item in selector:
 
-                            link = item.xpath(i)[0].get('href')
+                            link = item.xpath(i)[0].get("href")
 
                             self.linklist.append(link)
                     except IndexError:
@@ -181,36 +199,44 @@ AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36 Edg
                     finally:
                         continue
 
-        
-            
-
         else:
-            self.engine_params['quark']['q'] = self.keyword
-            for page in range(1, self.page_num+1):
-                self.engine_params['quark']['page'] = page
+            self.engine_params["quark"]["q"] = self.keyword
+            for page in range(1, self.page_num + 1):
+                self.engine_params["quark"]["page"] = page
 
                 r = requests.get(
-                    url=self.url, headers=self.headers, params=self.engine_params['quark'])
+                    url=self.url,
+                    headers=self.headers,
+                    params=self.engine_params["quark"],
+                )
 
                 t = etree.HTML(r.content)
-                selector = t.xpath(self.linkXpath['quark'])
+                selector = t.xpath(self.linkXpath["quark"])
 
                 for item in selector:
-                    temp = item.xpath(self.subxpath['quark'])
+                    temp = item.xpath(self.subxpath["quark"])
                     if len(temp):
-                        link = temp[0].get('href')
+                        link = temp[0].get("href")
                         self.linklist.append(link)
 
         self.open_link(self.linklist)
         self.linklist.clear()
 
+    def AdvancedOptions(self):
+        conn = dialog()
+        conn.show()
+        conn.exec_()
+        self.config = conn.generate_cofigure()
+        # self.config = glo.get_value('config')
+        print(self.config)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     root = QFileInfo(__file__).absolutePath()
     window = main()
-    window.setWindowTitle('SearchEngine')
-    window.setWindowIcon(QIcon(root+'/xhy.png'))
+    window.setWindowTitle("SearchEngine")
+    window.setWindowIcon(QIcon(root + "/xhy.png"))
     window.show()
     window.backgroundpic.update()
     sys.exit(app.exec_())
